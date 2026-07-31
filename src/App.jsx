d5b5ead5dc197172b0437e63a09ec34ef91544ca
Mu Ashraful Alam <ashraful442002@gmail.com>
2026-07-31 23:01:50 +0600

    Setup Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@
 
 function App() {
   return (
-    <div>
-      <h1>SkillSwap</h1>
+    <div className="min-h-screen flex items-center justify-center bg-slate-100">
+      <h1 className="text-5xl font-bold text-blue-600">
+        SkillSwap
+      </h1>
     </div>
   );
 }
